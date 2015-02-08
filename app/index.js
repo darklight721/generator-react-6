0fc51bf065ab51fc3a889671cbb838f350b7203b
Roy Evan Sia <roysia2004@gmail.com>
2015-02-08 23:47:52 +0800

    Update generator to use latest version and remove unnecessary files

--- app/index.js
+++ app/index.js
@@ -1,124 +1,94 @@
 'use strict';
 
-var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
+var yosay = require('yosay');
 
+var React6Generator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
 
-var ReactGulpBrowserifyGenerator = yeoman.generators.Base.extend({
-    init: function () {
-        this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+  prompting: function () {
+    var done = this.async();
 
-        this.on('end', function () {
-            if (!this.options['skip-install']) {
-                this.installDependencies();
-            }
-        });
+    this.log(yosay(
+      'You\'re using the fantastic ' + chalk.red('React-6') + ' generator. Write React apps in es6 now!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'project',
+      message: 'What is this project\'s name?',
+      default: this.appname
     },
+    {
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'Sass with Compass',
+        value: 'includeSass',
+        checked: true
+      }, {
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      }, {
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: true
+      }, {
+        name: 'Jest for unit tests',
+        value: 'includeJest',
+        checked: true
+      }]
+    }];
 
-    askFor: function () {
-        var done = this.async();
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
 
-        // have Yeoman greet the user
-        console.log(this.yeoman);
+      this.projectName = answers.project;
 
-        // replace it with a short and sweet description of your generator
-        console.log(chalk.magenta('You\'re using the fantastic React generator. We provide you full JavaScript solution with Sass support!'));
+      function hasFeature(feat) { return features.indexOf(feat) !== -1; }
 
-        var prompts = [{
-            name: 'project',
-            message: 'What is this project\'s name?'
-        },
-        {
-            type: 'checkbox',
-            name: 'features',
-            message: 'What more would you like?',
-            choices: [{
-                name: 'Sass with Compass',
-                value: 'includeSass',
-                checked: true
-            }, {
-                name: 'Bootstrap',
-                value: 'includeBootstrap',
-                checked: true
-            }, {
-                name: 'Modernizr',
-                value: 'includeModernizr',
-                checked: true
-            }, {
-                name: 'HTML template - Jade',
-                value: 'includeJade',
-                checked: true
-            }, {
-                name: 'CoffeeScript for JavaScript',
-                value: 'includeCoffeeScript',
-                checked: true
-            }, {
-                name: 'Jest for unit tests',
-                value: 'includeJest',
-                checked: true
-            }]
-        }];
+      this.includeSass = hasFeature('includeSass');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeModernizr = hasFeature('includeModernizr');
+      this.includeJest = hasFeature('includeJest');
 
-        this.prompt(prompts, function (answers) {
-            var features = answers.features;
+      done();
+    }.bind(this));
+  },
 
-            this.projectName = answers.project || 'myApp';
+  writing: function() {
+    this._copyTpl('_package.json', 'package.json');
+    this._copyTpl('_gulpfile.js', 'gulpfile.js');
+    this._copyTpl('_bower.json', 'bower.json');
+    this._copy('bowerrc', '.bowerrc');
+    this._copy('gitignore', '.gitignore');
 
-            function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+    this._copyTpl('index.html', 'app/index.html');
+    this._copy('favicon.ico', 'app/favicon.ico');
+    this._copy('robots.txt', 'app/robots.txt');
 
-            // manually deal with the response, get back and store the results.
-            // we change a bit this way of doing to automatically do this in the self.prompt() method.
-            this.includeSass = hasFeature('includeSass');
-            this.includeBootstrap = hasFeature('includeBootstrap');
-            this.includeModernizr = hasFeature('includeModernizr');
-            this.includeJade = hasFeature('includeJade');
-            this.includeCoffeeScript = hasFeature('includeCoffeeScript');
-            this.includeJest = hasFeature('includeJest');
+    this._copyTpl('main.scss', 'app/styles/main.scss');
+    this._copy('app.js', 'app/scripts/app.js');
+  },
 
-            done();
-        }.bind(this));
-    },
+  _copy: function(from, to) {
+    this.fs.copy(this.templatePath(from), this.destinationPath(to));
+  },
 
-    app: function () {
-        this.mkdir('app');
-        this.mkdir('app/scripts');
-        this.mkdir('app/scripts/ui');
-        if (this.includeJest) {
-            this.mkdir('app/scripts/ui/__tests__');
-        }
-        this.mkdir('app/styles');
-        this.mkdir('app/images');
+  _copyTpl: function() {
+    this.fs.copyTpl(this.templatePath(from), this.destinationPath(to), this);
+  },
 
-        this.template('_package.json', 'package.json');
-        this.template('_gulpfile.js', 'gulpfile.js');
-        this.template('_bower.json', 'bower.json');
-        this.template('main.scss', 'app/styles/main.scss');
-
-        this.copy('index.html', 'app/index.html');
-
-        if (this.includeCoffeeScript) {
-            this.copy('app.coffee', 'app/scripts/app.coffee');
-            this.copy('ui/Timer.coffee', 'app/scripts/ui/Timer.coffee');
-        } else {
-            this.copy('app.js', 'app/scripts/app.js');
-            this.copy('ui/Timer.js', 'app/scripts/ui/Timer.js');
-        }
-
-        if (this.includeJest) {
-            this.copy('ui/__tests__/Timer-test.js', 'app/scripts/ui/__tests__/Timer-test.js');
-        }
-        this.copy('favicon.ico', 'app/favicon.ico');
-        this.copy('bowerrc', '.bowerrc');
-        this.copy('gitignore', '.gitignore');
-        this.copy('robots.txt', 'app/robots.txt');
-
-    },
-
-    projectfiles: function () {
-        this.copy('editorconfig', '.editorconfig');
-        this.copy('jshintrc', '.jshintrc');
-    }
+  install: function() {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    });
+  }
 });
 
-module.exports = ReactGulpBrowserifyGenerator;
+module.exports = React6Generator;
